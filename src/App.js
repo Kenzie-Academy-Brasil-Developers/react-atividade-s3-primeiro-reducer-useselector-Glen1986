6f27186c09ff234ae4e9c5d1482554f952f3a0cb
glen1986 <escobar.glen@gmail.com>
2021-09-27 22:16:00 -0300

    frutas

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FruitsPage from './pages/fruitPages';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FruitsPage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
